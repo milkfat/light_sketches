fb2656a51e6a1e73b7b903f63d3f2a33b70817db
matt <matt@matt-mbp.local>
2020-06-25 11:59:48 -0700

    some optimizing attempted some sort of progressive rendering

--- mandlebrot.h
+++ mandlebrot.h
@@ -32,9 +32,15 @@ class MANDLEBROT: public LIGHT_SKETCH {
 #endif
 
     uint32_t sample_cnt = 0;
-    uint32_t Ivals[NUM_LEDS] = {0};
-    uint16_t samples_taken[NUM_LEDS] = {0};
-    uint8_t samples_remaining[NUM_LEDS] = {1};
+
+    struct PIXEL {
+        uint32_t ival = 0;
+        uint16_t samples_taken = 0;
+        uint8_t samples_remaining = 1;
+        uint8_t done = 0;
+    };
+
+    PIXEL pixels[NUM_LEDS];
     bool reset_pixels = 0;
     bool was_moving = 0;
     uint16_t iterations = 64;
@@ -68,7 +74,7 @@ class MANDLEBROT: public LIGHT_SKETCH {
         control_variables.add(move_right,"Right", 1, 39);
         control_variables.add(move_down,"Down", 1, 40);
         control_variables.add(iterations,"Iterations", 1, 2048);
-        control_variables.add(layer_frequency,"Layer Frequency", 1, 256);
+        control_variables.add(layer_frequency,"Layer Frequency", 1, 4096);
         control_variables.add(user_color_shift,"Color Shift", 0, 512);
         control_variables.add(auto_color_shift,"Auto Shift");
         control_variables.add(auto_color_shift_speed,"Color Shift Speed", -256, 256);
@@ -77,7 +83,7 @@ class MANDLEBROT: public LIGHT_SKETCH {
         control_variables.add(color2,"Color 2");
         control_variables.add(color3,"Color 3");
         for (int i = 0; i < NUM_LEDS; i++) {
-            samples_remaining[i] = 1;
+            pixels[i].samples_remaining = 1;
         }
         
     }
@@ -181,37 +187,46 @@ class MANDLEBROT: public LIGHT_SKETCH {
                 x_rand = ((random(INT32_MAX)-INT32_MAX/2)*x_pitch*2)/INT32_MAX;
                 y_rand = ((random(INT32_MAX)-INT32_MAX/2)*y_pitch*2)/INT32_MAX;
             }
-            static int start_line = MATRIX_WIDTH/2;
-            static int line = MATRIX_WIDTH/2;
-            static int jump = MATRIX_WIDTH;
-            if (reset_pixels) {
-                start_line = MATRIX_WIDTH/2;
-                line = MATRIX_WIDTH/2;
-                jump = MATRIX_WIDTH;
-            }
-            while (millis() < stop_time) {
-                int Px = line;
-                line += jump;
-                if (line > MATRIX_WIDTH-1) {
-                    line%=MATRIX_WIDTH;
-                    jump/=2;
-                    line-=jump/2;
-                    if (jump == start_line) {
-                        start_line /= 2;
-                        line = start_line;
-                        jump = MATRIX_WIDTH/2;
+            const int largest_dimension = msb(_min(MATRIX_HEIGHT,MATRIX_WIDTH));
+            const int initial_jump = largest_dimension*2;
 
-                        if (jump == 1) {
-                            start_line = MATRIX_WIDTH/2;
-                            line = MATRIX_WIDTH/2;
-                            jump = MATRIX_WIDTH;
+            static int jump_x = initial_jump;
+            static int jump_y = initial_jump;
+
+            if (reset_pixels) {
+                jump_x = initial_jump;
+                jump_y = initial_jump;
+                for (int i = 0; i < NUM_LEDS; i++) {
+                    pixels[i].done = 0;
+                }
+            }
+
+            while (millis() < stop_time) {
+                static int Py = (jump_y/2)%largest_dimension;
+
+                if (Py >= MATRIX_HEIGHT) {
+                    if (jump_x > jump_y) {
+                        jump_x /= 2;
+                    } else {
+                        jump_y /= 2;
+                        jump_x = initial_jump;
+                    }
+
+                    if (!jump_y) {
+                        jump_y = initial_jump;
+                        jump_x = initial_jump;
+                        for (int i = 0; i < NUM_LEDS; i++) {
+                            pixels[i].done = 0;
                         }
                     }
-                    Px = line;
+                    Py = (jump_y/2)%largest_dimension;
                 }
-                for (int Py = 0; Py < MATRIX_HEIGHT; Py++) {
-                    int led = XY(Px,Py);
-                    if(samples_remaining[led]==0 || samples_taken[led]==65535) continue;
+
+                int led = XY((jump_x/2)%largest_dimension,Py) - jump_x;
+                for (int Px = (jump_x/2)%largest_dimension; Px < MATRIX_WIDTH; Px+=jump_x) {
+                    led+=jump_x;
+                    if(pixels[led].done || pixels[led].samples_remaining==0 || pixels[led].samples_taken==65535) continue;
+                    pixels[led].done = 1;
                     //calculate our position
 #ifdef DOUBLE_PRECISION
                     double x0 = (Px+MIN_DIMENSION/2-MATRIX_WIDTH/2)*x_pitch + x_rand - 1.75;
@@ -247,34 +262,82 @@ class MANDLEBROT: public LIGHT_SKETCH {
                         y = 2*x*y + y0;
                         x = xtemp;
                     }
-                    Ivals[led] += iteration;
-                    samples_taken[led]++;
-                    if(Ivals[led] == 0) {
-                        samples_remaining[led] = 0;
+                    // int last_avg = 0;
+                    // if(pixels[led].samples_taken) {
+                    //     last_avg = pixels[led].ival/pixels[led].samples_taken;
+                    // }
+                    pixels[led].ival += iteration;
+                    pixels[led].samples_taken++;
+                    //int avg = pixels[led].ival/pixels[led].samples_taken;
+                    if(pixels[led].ival == 0) {
+                    //if(last_avg == avg) {
+                        //optimization for two possible reasons:
+                        // 1) we reached max iteration
+                        // 2) our average sample value did not change
+                        //don't sample this pixel or adjacent pixels next time
+                        pixels[led].samples_remaining = 0;
+                        // if (led > 0) {
+                        //     pixels[led-1].samples_remaining = 0;
+                        //     if (led >= MATRIX_WIDTH) {
+                        //         pixels[led-MATRIX_WIDTH].samples_remaining = 0;
+                        //     }
+                        // }
+                        // if (led < MATRIX_WIDTH-1) {
+                        //     pixels[led+1].samples_remaining = 0;
+                        //     if (led < (MATRIX_WIDTH*MATRIX_HEIGHT)-MATRIX_WIDTH) {
+                        //         pixels[led+MATRIX_WIDTH].samples_remaining = 0;
+                        //     }
+                        // }
                     } else {
-                        samples_remaining[XY(Px+1,Py)] = 1;
-                        samples_remaining[XY(Px-1,Py)] = 1;
-                        samples_remaining[XY(Px,Py+1)] = 1;
-                        samples_remaining[XY(Px,Py-1)] = 1;
+                        //do sample this pixel and adjacent pixels next time
+                        if (led > 0) {
+                            pixels[led-1].samples_remaining = 1;
+                            if (led >= MATRIX_WIDTH) {
+                                pixels[led-MATRIX_WIDTH].samples_remaining = 1;
+                            }
+                        }
+                        if (led < MATRIX_WIDTH-1) {
+                            pixels[led+1].samples_remaining = 1;
+                            if (led < (MATRIX_WIDTH*MATRIX_HEIGHT)-MATRIX_WIDTH) {
+                                pixels[led+MATRIX_WIDTH].samples_remaining = 1;
+                            }
+                        }
                     }
                 }
+                Py += jump_y;
             }
             sample_cnt++;
         }
 
         //draw our accumulated data to the screen
-        CRGB old_color = CRGB(0,0,0);
+        bool use_top = 0;
+        bool use_left = 0;
         for (int i = 0; i < NUM_LEDS; i++) {
-            if(!Ivals[i]) {
-                if (samples_taken[i]) {
-                    old_color = CRGB(0,0,0);
-                } else {
-                    leds[i] = old_color;
+            if ( !(i%MATRIX_WIDTH) ) {
+                if (pixels[i].samples_taken) {
+                    use_left = 1;
+                    use_top = 0;
+                } else if (i >= MATRIX_WIDTH) {
+                    use_left = 0;
+                    use_top = 1;
                 }
+            }
+            if(!pixels[i].samples_taken) {
+                if (use_top) {
+                    leds[i] = leds[i-MATRIX_WIDTH];
+                } else if (use_left) {
+                    leds[i] = leds[i-1];
+                }
+                // else if (pixels[i-MATRIX_WIDTH].samples_taken) {
+                //    leds[i] = leds[i-MATRIX_WIDTH];
+                // }
                 continue;
             }
-            uint32_t temp0 = Ivals[i];
-            temp0/=samples_taken[i];
+            if(!pixels[i].ival) {
+                continue;
+            }
+            uint32_t temp0 = pixels[i].ival;
+            temp0/=pixels[i].samples_taken;
             uint16_t depth = temp0%=iterations; //save this for later
             temp0 = iterations - temp0;
             temp0+=color_shift;
@@ -314,7 +377,6 @@ class MANDLEBROT: public LIGHT_SKETCH {
                 c3 = layer_tightness - c3;
                 nblend(leds[i],color3,(c3*255*depth)/(layer_tightness*iterations));
             }
-            old_color = leds[i];
         }
 
 
@@ -378,24 +440,21 @@ class MANDLEBROT: public LIGHT_SKETCH {
         
 
         //reset if zoom or position or number of iterations changes
-        if (reset_pixels || old_iterations != iterations) {
+        if (old_iterations != iterations) {
+            reset_pixels=1;
+        }
+        if (reset_pixels) {
             was_moving = 1;
             sample_cnt = 0;
-            for (int x = 0; x < MATRIX_WIDTH; x++) {
-                for (int y = 0; y < MATRIX_HEIGHT; y++) {
-                    int i = XY(x,y);
-                    samples_remaining[i] = 1;
-                    samples_taken[i] = 0;
-                    Ivals[i] = 0;
-                }
+            for (int i = 0; i < NUM_LEDS; i++) {
+                pixels[i].samples_remaining = 1;
+                pixels[i].samples_taken = 0;
+                pixels[i].ival = 0;
             }
         } else if (was_moving == 1) {
             was_moving = 0;
-            for (int x = 0; x < MATRIX_WIDTH; x++) {
-                for (int y = 0; y < MATRIX_HEIGHT; y++) {
-                    int i = XY(x,y);
-                    samples_remaining[i] = 1;
-                }
+            for (int i = 0; i < NUM_LEDS; i++) {
+                pixels[i].samples_remaining = 1;
             }
         }
 
